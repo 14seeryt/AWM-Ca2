48e46dd6d9d59d3dc466c8504e73290509143c3f
14seeryt <57757387+14seeryt@users.noreply.github.com>
2022-12-10 22:47:36 +0000

    Update App.js
    Adding leaf icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
-import { MapContainer, TileLayer} from 'react-leaflet';
-import { L } from 'leaflet';
+import React, {useState} from 'react'
+import axios from 'axios';
+import { Icon } from 'leaflet';
+import { Alert, Spinner } from 'react-bootstrap';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import useSWR from 'swr';
 import "./App.css"
+
+export const icon = new Icon({
+    iconUrl: "leaf-green.png",
+    shadowUrl: "leaf-shadow.png",
+    iconSize: [38, 95],
+    shadowSize: [50, 64],
+    iconAnchor: [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -76],
+ });
 
 const App = () => {
     return (
