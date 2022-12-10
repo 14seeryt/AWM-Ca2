d512d3e9139fa1df8225321011ca084a9ba77e90
14seeryt <57757387+14seeryt@users.noreply.github.com>
2022-12-10 22:41:47 +0000

    Update App.js
    Map of Dublin

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,14 @@ import { L } from 'leaflet';
 import "./App.css"
 
 const App = () => {
-
+    return (
+    <MapContainer center={[53.3498, -6.2603]} zoom = {9}>    
+        <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+         />
+    </MapContainer>
+    )
 }
 
 export default App
